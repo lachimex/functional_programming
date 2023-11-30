3f76c807ca0d5246b20e4c3b9803005d4639bae1
Michał Dydek <dydmichal@gmail.com>
2023-11-30 12:20:44 +0100

    lab3

--- lab3/ex2.hs
+++ lab3/ex2.hs
@@ -15,3 +15,12 @@ sumSqr = sumWith(^2)
 sumCube = sumWith(^3)
 sumAbs = sumWith abs
 listLength = sumWith (\x -> 1)
+
+a = foldr (+) 0 . map(^2) . filter ((==1) . (`mod` 3) ). map(+1) . map(*2) $ [0..7]
+
+f1 :: (Integer -> b) -> Integer -> [b]
+f1 = \g -> map g . (\x -> map ($ x) [id, (+1), (+2)])
+
+fun :: String -> Int
+fun = length . filter ((>2) . length) . map (filter (`elem` vowels)) . words
+      where vowels = "aeiouy"
